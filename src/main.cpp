4e147af0f24c15342d4c84c84a3e06bf49c3b08e
I2000C <prueb123i@outlook.es>
2026-09-14 13:20:09 +0200

    Added buttons and pedals testing mode if MUTE button is pressed when ESP32 is turned ON

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,83 @@ void mainTask(void* args) {
     }
 }
 
+// Test buttons and pedals
+void testButtonsAndPedals() {
+    VehicleState vehicleState;
+
+    while(1) {
+        bool turnOnLed = false;
+
+        // Check pressed button
+        Button button = Buttons::getPressedButton();
+        switch(button) {
+            case Button::BUTTON_REJECT_CALL:
+                Serial.println("Button pressed: BUTTON_REJECT_CALL");
+                turnOnLed = true;
+                break;
+            case Button::BUTTON_MUTE:
+                Serial.println("Button pressed: BUTTON_MUTE");
+                turnOnLed = true;
+                break;
+            case Button::BUTTON_ANSWER_CALL:
+                Serial.println("Button pressed: BUTTON_ANSWER_CALL");
+                turnOnLed = true;
+                break;
+            case Button::BUTTON_NEXT_TRACK:
+                Serial.println("Button pressed: BUTTON_NEXT_TRACK");
+                turnOnLed = true;
+                break;
+            case Button::BUTTON_PREVIOUS_TRACK:
+                Serial.println("Button pressed: BUTTON_PREVIOUS_TRACK");
+                turnOnLed = true;
+                break;
+            case Button::BUTTON_VOLUME_DOWN:
+                Serial.println("Button pressed: BUTTON_VOLUME_DOWN");
+                turnOnLed = true;
+                break;
+            case Button::BUTTON_VOLUME_UP:
+                Serial.println("Button pressed: BUTTON_VOLUME_UP");
+                turnOnLed = true;
+                break;
+        }
+
+        // Check brake pedal
+        if(BrakeClutch::isBrakePressed()) {
+            Serial.println("Pedal pressed: BRAKE");
+            turnOnLed = true;
+        }
+
+        // Check clutch pedal
+        if(BrakeClutch::isClutchPressed()) {
+            Serial.println("Pedal pressed: CLUTCH");
+            turnOnLed = true;
+        }
+
+        // Read vehicle state
+        canReader.readState(vehicleState);
+
+        // Check parking brake
+        if(vehicleState.parkingBrake) {
+            Serial.println("Lever pressed: PARKING_BRAKE");
+            turnOnLed = true;
+        }
+
+        // Check reverse light
+        if(vehicleState.reverseLight) {
+            Serial.println("Light activated: REVERSE_LIGHT");
+            turnOnLed = true;
+        }
+
+        if(turnOnLed) {
+            IndicatorLed::setState(SystemState::STATE_ACTIVE);
+        } else {
+            IndicatorLed::setState(SystemState::STATE_OFF);
+        }
+
+        delay(MAIN_LOOP_PERIOD_MS);
+    }
+}
+
 // Fatal error handler: blinks an error code indefinitely using the indicator LED
 void fatalErrorLoop(uint8_t code) {
     while(true) {
@@ -95,17 +172,33 @@ void setup() {
             delay(BUTTONS_QUERY_PERIOD_MS);
             buttonDebugTime += BUTTONS_QUERY_PERIOD_MS;
         } else {
-            // Enable debug mode
-            Debug::setEnabled(true);
-            Serial.begin(SERIAL_DEBUG_BAUDRATE);
-            Debug::println("Debug mode is enabled");
+            if(button == Button::BUTTON_MUTE) {
+                // Enable button and pedals testing mode
+                Debug::setEnabled(true);
+                Serial.begin(SERIAL_DEBUG_BAUDRATE);
+                Debug::println("Buttons debug mode is enabled");
 
-            // Fade indicator LED for 2 seconds
-            for(uint32_t time = 0; time < 2000; time += MAIN_LOOP_PERIOD_MS) {
-                IndicatorLed::setState(SystemState::STATE_OVERRIDE);
-                delay(MAIN_LOOP_PERIOD_MS);
+                // Fade indicator LED for 4 seconds
+                for(uint32_t time = 0; time < 4000; time += MAIN_LOOP_PERIOD_MS) {
+                    IndicatorLed::setState(SystemState::STATE_OVERRIDE);
+                    delay(MAIN_LOOP_PERIOD_MS);
+                }
+
+                IndicatorLed::setState(SystemState::STATE_OFF);
+                testButtonsAndPedals();
+            } else {
+                // Enable debug mode
+                Debug::setEnabled(true);
+                Serial.begin(SERIAL_DEBUG_BAUDRATE);
+                Debug::println("Debug mode is enabled");
+    
+                // Fade indicator LED for 2 seconds
+                for(uint32_t time = 0; time < 2000; time += MAIN_LOOP_PERIOD_MS) {
+                    IndicatorLed::setState(SystemState::STATE_OVERRIDE);
+                    delay(MAIN_LOOP_PERIOD_MS);
+                }
+                break;
             }
-            break;
         }
     }
 
